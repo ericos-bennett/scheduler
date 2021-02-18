72dcd6e0c768d82e353b6d9a2ffe82502bdf66f1
Eric <ericos.bennett@gmail.com>
2021-02-17 21:29:39 -0800

    add getAppointmentForDay selector

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -6,65 +6,71 @@ import "components/Application.scss";
 import DayList from 'components/DayList';
 import Appointment from 'components/Appointment';
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm"
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "John Bonham",
-      interviewer: {
-        id: 4, 
-        name: "Cohana Roy", 
-        avatar: "https://i.imgur.com/FK8V841.jpg"
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Red Scarlet",
-      interviewer: {
-        id: 5, 
-        name: "Sven Jones", 
-        avatar: "https://i.imgur.com/twYrpay.jpg"
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-    interview: {
-      student: "Val Nature",
-      interviewer: {
-        id: 3, 
-        name: "Mildred Nazir", 
-        avatar: "https://i.imgur.com/T2WwVfS.png"
-      }
-    }
-  },
-];
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm"
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: "2pm",
+//     interview: {
+//       student: "John Bonham",
+//       interviewer: {
+//         id: 4, 
+//         name: "Cohana Roy", 
+//         avatar: "https://i.imgur.com/FK8V841.jpg"
+//       }
+//     }
+//   },
+//   {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Red Scarlet",
+//       interviewer: {
+//         id: 5, 
+//         name: "Sven Jones", 
+//         avatar: "https://i.imgur.com/twYrpay.jpg"
+//       }
+//     }
+//   },
+//   {
+//     id: 5,
+//     time: "4pm",
+//     interview: {
+//       student: "Val Nature",
+//       interviewer: {
+//         id: 3, 
+//         name: "Mildred Nazir", 
+//         avatar: "https://i.imgur.com/T2WwVfS.png"
+//       }
+//     }
+//   },
+// ];
 
 const Application = props => {
   
-  const [days, setDays] = useState([]);
   const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
+
+  const [state, setState] = useState({
+    day: 'Monday',
+    days: [],
+    appointments: {}
+  });
 
   useEffect(() => {
     axios.get('/api/days')
