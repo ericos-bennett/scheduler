7ad41ca0a89ea57112363df1e0afc9be3ff24ee6
Eric <ericos.bennett@gmail.com>
2021-02-23 18:42:51 -0800

    add integration test for booking a new appt

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,19 +1,54 @@
 import React from "react";
 
-import { render, cleanup, fireEvent, waitForElement } from "@testing-library/react";
+import { 
+  render, 
+  cleanup, 
+  fireEvent, 
+  waitForElement,
+  getByText,
+  getAllByTestId,
+  getByAltText,
+  getByPlaceholderText,
+  prettyDOM
+ } from "@testing-library/react";
 
 import Application from "components/Application";
 
-afterEach(cleanup);
+describe('Application', () => {
 
-it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+  afterEach(cleanup);
+  
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+  
+    const { getByText } = render(<Application />);
+  
+    await waitForElement(() => getByText('Monday'))
+   
+    fireEvent.click(getByText('Tuesday'));
+   
+    expect(getByText('Leopold Silvers')).toBeInTheDocument();
+  
+  });
+  
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+  
+    const { container } = render(<Application />);
+    
+    await waitForElement(() => getByText(container, 'Archie Cohen'));
+    
+    const appointment = getAllByTestId(container, "appointment")[0];
+    console.log(prettyDOM(appointment));
 
-  const { getByText } = render(<Application />);
+    fireEvent.click(getByAltText(appointment, 'Add'));
 
-  await waitForElement(() => getByText('Monday'))
- 
-  fireEvent.click(getByText('Tuesday'));
- 
-  expect(getByText('Leopold Silvers')).toBeInTheDocument();
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, 'Save'));
+
+  });
 
 });
